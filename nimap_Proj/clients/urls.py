d44cffea59e37c49d4b572aba07a9e367b00356d
saifsayed786 <42892558+saifsayed786@users.noreply.github.com>
2021-07-11 18:56:42 +0530

    initial commit

---
+++ nimap_Proj/clients/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name = 'home'),
+    path('showClients/',views.clients_list,name = 'client_list'),
+    path('showProjects/',views.project_list,name = 'project_list'),
+    path('create_clients/',views.CreateClients,name = 'create_clients'),
+    path('create_projects/',views.CreateProjects,name = 'create_projects'),
+    path('update_projects/<str:pk>/',views.updateProjects,name = 'update_projects'),
+    path('delete_projects/<str:pk>/',views.deleteProject,name = 'delete_projects'),
+    path('myview',views.myview,name = 'myview')
+]
